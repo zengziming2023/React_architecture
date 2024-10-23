52f66d2cb24e8d0bae303ae81bd5171b646a08ae
Nick2022 <876927113@qq.com>
2024-10-23 15:47:04 +0800

    feat: add ant-mobile for ui.

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,17 +1,18 @@
 import {useNavigate} from "react-router-dom";
 
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {RouterNames} from "../../routes/routers.ts";
 import './home.less'
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, RootState} from "../../redux/store.ts";
 import {incrementByAmount} from "../../redux/counterSlice.ts";
+import {Button, Input, Toast} from "antd-mobile";
 
 const Home = () => {
     const reduxCount = useSelector((state: RootState) => state.counter.count)
     // const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
     const dispatch = useDispatch<AppDispatch>();
-
+    const [value, setValue] = useState('')
 
     // const [count, setCount] = useState(0);
     const navigate = useNavigate()
@@ -33,10 +34,18 @@ const Home = () => {
         }, 500)
     }
 
+    const handleClick = () => {
+        Toast.show(`You enter: ${value}`)
+    }
+
+
     return (
         <div className='home'>
             <h2>Home</h2>
             <button onClick={jump2AboutPage}>Jump 2 About page: {reduxCount}</button>
+            <Input className='input' placeholder='Enter something' value={value} onChange={setValue} clearable={true}/>
+            <Button className={'button'} color={"primary"} size={"large"} onClick={handleClick}>Show
+                Toast</Button>
         </div>
     )
 }
