ee6a3833da4e35caba71dcdaba81fee6d81d0a14
Nick2022 <876927113@qq.com>
2024-11-11 15:04:34 +0800

    feat: practise ahook, useRequest.

--- src/features/ahooksDemo/AHooksDemo.tsx
+++ src/features/ahooksDemo/AHooksDemo.tsx
@@ -1,14 +1,35 @@
 import "./ahooks_demo.less"
-import {useToggle} from "ahooks";
+import {useRequest, useToggle} from "ahooks";
 import {Button} from "antd-mobile";
+import {Product} from "../../network/response/sys/Product.ts";
+import {BLResponse} from "../../network/response/BLResponse.ts"
+import {getProductList} from "../../network/api/sysApi.ts";
 
 const AHooksDemo = () => {
     const [state, {toggle}] = useToggle()
+    const {
+        loading,
+        data,
+        runAsync
+    } = useRequest<(BLResponse<Product[]> | undefined), []>(() => {
+        return getProductList()
+    }, {
+        manual: true,
+    })
 
     return (<>
         <div className="ahooks-demo">
             <span>current state: {String(state)}</span>
             <Button onClick={toggle}>Toggle state</Button>
+            <Button onClick={async () => {
+                await runAsync()
+            }}>Test UseRequest.</Button>
+            {
+                loading ? <span>loading...</span> : null
+            }
+            {
+                (!loading && data != undefined) ? <span>{JSON.stringify(data)}</span> : null
+            }
         </div>
     </>)
 }
