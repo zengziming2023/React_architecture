616895f4335e9475111a3a969ca0a5176298e9c8
Nick2022 <876927113@qq.com>
2024-11-12 15:05:17 +0800

    feat: practise ahook useRequest.

--- src/features/ahooksDemo/AHooksDemo.tsx
+++ src/features/ahooksDemo/AHooksDemo.tsx
@@ -10,11 +10,35 @@ const AHooksDemo = () => {
     const {
         loading,
         data,
-        runAsync
+        runAsync,
+        cancel,
     } = useRequest<(BLResponse<Product[]> | undefined), []>(() => {
         return getProductList()
     }, {
-        manual: true,
+        manual: false,
+        ready: state,
+        loadingDelay: 200,      // 200ms 内返回了数据，不会展示loading. 避免闪烁
+        pollingInterval: 3000,
+        pollingErrorRetryCount: 3,
+        pollingWhenHidden: false,   // 页面隐藏时，轮询暂停
+        defaultParams: [],
+        refreshDeps: [],     // 依赖刷新，如果入参变化，自动刷新。
+        refreshOnWindowFocus: true,     // 屏幕聚焦自动刷新
+        focusTimespan: 5000,  // 重新请求间隔
+        debounceWait: 300,  // 防抖
+        retryCount: 3,      // 自动重试3次
+        onBefore: (params: []) => {
+            console.log(params, "onBefore");
+        },
+        onSuccess: (data?: BLResponse<Product[]>) => {
+            console.log(data, "onSuccess");
+        },
+        onError: (error: Error, params: []) => {
+            console.log(error, params, "onError");
+        },
+        onFinally: (params: [], data?: BLResponse<Product[]>) => {
+            console.log(params, data, "onFinally");
+        }
     })
 
     return (<>
@@ -23,7 +47,8 @@ const AHooksDemo = () => {
             <Button onClick={toggle}>Toggle state</Button>
             <Button onClick={async () => {
                 await runAsync()
-            }}>Test UseRequest.</Button>
+            }}>Test UseRequest.(start timing.)</Button>
+            <Button onClick={cancel}>Cancel request timing.</Button>
             {
                 loading ? <span>loading...</span> : null
             }
